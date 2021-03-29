913544746c07119ddcd0ca53294c6d2f64d851d0
Richard R <58728519+rrjbca@users.noreply.github.com>
2021-03-29 17:24:42 +0100

    Update CAMB and CLASS tests to use power spectrum interpolators

--- skypy/power_spectrum/tests/test_class.py
+++ skypy/power_spectrum/tests/test_class.py
@@ -23,24 +23,27 @@ def test_classy():
     '''
     Test a default astropy cosmology
     '''
-    from skypy.power_spectrum import classy
+    from skypy.power_spectrum import CLASSY
 
+    # Setup CLASS interpolator
     Pl15massless = Planck15.clone(name='Planck 15 massless neutrino', m_nu=[0., 0., 0.]*u.eV)
+    k_max, z_grid = 10, np.array([0, 1])
+    ps = CLASSY(k_max, z_grid, Pl15massless)
 
     redshift = [0.0, 1.0]
     wavenumber = np.logspace(-4.0, np.log10(2.0), 200)
-    pzk = classy(wavenumber, redshift, Pl15massless)
+    pzk = ps(wavenumber, redshift)
     assert pzk.shape == (len(redshift), len(wavenumber))
     assert allclose(pzk, test_pzk, rtol=1.e-4)
 
     # also check redshifts are ordered correctly
     redshift = [1.0, 0.0]
-    pzk = classy(wavenumber, redshift, Pl15massless)
+    pzk = ps(wavenumber, redshift)
     assert pzk.shape == (len(redshift), len(wavenumber))
     assert allclose(pzk, test_pzk[np.argsort(redshift)], rtol=1.e-4)
 
     # also check scalar arguments are treated correctly
     redshift = 1.0
     wavenumber = 1.e-1
-    pzk = classy(wavenumber, redshift, Pl15massless)
+    pzk = ps(wavenumber, redshift)
     assert np.isscalar(pzk)
